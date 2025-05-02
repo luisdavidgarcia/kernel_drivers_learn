284b5c9843744f4c22950ee6f4d71a2f9cb9cb6b
luisdavidgarcia <hookeri_bartz@aleeas.com>
2025-05-01 17:57:59 -0700

    Hello World module is more descriptive

---
+++ raspberrypi/02_better_hello/hello.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init my_init(void)
+{
+ 	printk("hello - Hello, Kernel!\n");
+ 	return 0;  
+}
+
+static void __exit my_exit(void)
+{
+	printk("hello - Goodbye, Kernel!\n");
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Luis David Garcia");
+MODULE_DESCRIPTION("A simple Hello World Linux Kernel Module");
+
